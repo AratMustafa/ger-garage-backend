67c272f1bc15dd7144ddc69ddbd7965f2c30a27b
snap-balance <2022141@student.cct.ie>
2023-01-14 23:34:11 +0000

    created project repo

---
+++ src/main/java/com/demo/gerproject/controller/CustomerController.java
@@ -0,0 +1,72 @@
+package com.demo.gerproject.controller;
+import com.demo.gerproject.dao.UserVehiclesInServiceProjection;
+import com.demo.gerproject.dao.VehiclesInServiceProjection;
+import com.demo.gerproject.model.Customer;
+import com.demo.gerproject.model.Payment;
+import com.demo.gerproject.service.CustomerService;
+
+import lombok.AllArgsConstructor;
+
+import java.util.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+@AllArgsConstructor
+@CrossOrigin(origins="http://localhost:3000")
+public class CustomerController {
+
+	private final CustomerService customerService;
+	
+	@GetMapping
+	public ResponseEntity<List<Customer>> getCustomers(){
+		return new ResponseEntity<>(customerService.getCustomers(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Customer> getCustomer(@PathVariable int id) {
+		return new ResponseEntity<>(getCustomerById(id), HttpStatus.OK);
+	}
+
+	private Customer getCustomerById(int id) {
+		return customerService.getCustomerById(id);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
+		return new ResponseEntity<>(customerService.createCustomer(customer), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> getCustomer(@PathVariable int id, @RequestBody Customer customer){
+		Customer oldCustomer = getCustomerById(id);
+		oldCustomer.setFirstname(customer.getFirstname());
+		oldCustomer.setLastname(customer.getLastname());
+		oldCustomer.setPassword(customer.getPassword());
+		oldCustomer.setCreatedAt(new Date());
+		customerService.updateCustomer(id, oldCustomer);
+		return new ResponseEntity<>(null, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteCustomer(@PathVariable int id){
+		customerService.deleteCustomer(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@GetMapping("/userVehiclesInService/{id}")
+	public ResponseEntity<List<UserVehiclesInServiceProjection>> getUserVehiclesInService(@PathVariable int id){
+		return new ResponseEntity<>(customerService.getUserVehiclesInService(id), HttpStatus.OK);
+	}
+}
